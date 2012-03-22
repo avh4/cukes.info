38afe13cf094aa0ce59a7145101f2b1a70263029
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2012-03-22 19:32:13 +0000

    Properly remember what tabs the user selected

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,24 @@
 $(function () {
+    $('table').addClass('table').addClass('table-condensed').addClass('table-striped');
     $('.nav-tabs').find('a:first').tab('show');
-    if(window.localStorage.preferredTab) {
-//        $('.nav-tabs a:contains("' + window.localStorage.preferredTab + '")').tab('show');
+    
+    // Select preferred tabs
+    var preferredTabs;
+    try {
+        preferredTabs = JSON.parse(window.localStorage.getItem('preferredTabs'));
+    } catch(e) {
+        preferredTabs = {};
+    }
+    for(var preferredTab in preferredTabs) {
+        $('.nav-tabs a:contains("' + preferredTab + '")').tab('show');
     }
 
     $('a[data-toggle="tab"]').on('shown', function (e) {
-//        window.localStorage.preferredTab = e.target.innerText;
+        $(this).parent().siblings().find('a').each(function(_, a) {
+            var unpreferred = a.innerText;
+            delete preferredTabs[unpreferred];
+        });
+        preferredTabs[e.target.innerText] = e.target.innerText;
+        window.localStorage.setItem('preferredTabs', JSON.stringify(preferredTabs));
     });
-    
-    $('table').addClass('table').addClass('table-condensed').addClass('table-striped');
 });
